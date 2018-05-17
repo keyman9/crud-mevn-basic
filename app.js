c92899226050f98e1f24f496025f24e090cac182
keyman9 <laxscott@gmail.com>
2018-05-16 22:50:03 -0600

    Added book route, express configuration, and app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var parth = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+
+var book = require('./routes/book');
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({'extended':'false'}));
+app.use(express.static(path.join(__dirname, 'dist')));
+app.use('/books', express.static(path.join(__dirname, 'dist')));
+app.use('/book', book);
+
+//catch 404 and send to error handler
+app.use(function(req,res,next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use(function(err,req,res,next){
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  //render error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
